b23eb32134e54340143c40ae8866cc9a143d2f65
Niesha White <nieshawhite10@gmail.com>
2020-06-23 12:19:52 -0500

    starter code

--- js/script.js
+++ js/script.js
@@ -50,107 +50,57 @@ const cardsArray = [{
 ]
 
 // Grab the div with an id of root
-const game = document.getElementById('game')
+
 
 // Create a section with a class of grid
-const grid = document.createElement('section')
-grid.setAttribute('class', 'grid')
+
 
 // Duplicate array to create a match for each card
-let gameGrid = cardsArray.concat(cardsArray)
 
-let firstGuess = ''
-let secondGuess = ''
-let count = 0
-let previousTarget = null
-let delay = 1200
 
 // Append the grid section to the game div
-game.appendChild(grid)
+
 
 // Randomize game grid on each load
-gameGrid.sort(() => 0.5 - Math.random())
+
 
 // For each item in the cardsArray array...
-gameGrid.forEach(item => {
-    // Create card element with the name dataset
-    const card = document.createElement('div')
-    card.classList.add('card')
-    card.dataset.name = item.name
 
-    // Create front of card
-    const front = document.createElement('div')
-    front.classList.add('front')
+// Create card element with the name dataset
 
-    // Create back of card, which contains
-    const back = document.createElement('div')
-    back.classList.add('back')
-    back.style.backgroundImage = `url(${item.img})`
 
-    // Append card to grid, and front and back to each card
-    grid.appendChild(card)
-    card.appendChild(front)
-    card.appendChild(back)
-})
+// Create front of card
+
+
+// Create back of card, which contains
+
+
+// Append card to grid, and front and back to each card
+
 
 // Add event listener to grid
-grid.addEventListener('click', function(event) {
-    // The event target is our clicked item
-    let clicked = event.target
 
-    // Do not allow the grid section itself to be selected; only select divs inside the grid
-    if (clicked.nodeName === 'SECTION' || clicked === previousTarget ||
-        clicked.parentNode.classList.contains('selected')) {
-        return
-    }
+// The event target is our clicked item
 
-    // Add selected class
-    // ...
-    if (count < 2) {
-        count++
-        // Add selected class
-        if (count === 1) {
-            // Assign first guess
-            firstGuess = clicked.parentNode.dataset.name
-            console.log(firstGuess)
-            clicked.parentNode.classList.add('selected')
-        } else {
-            // Assign second guess
-            secondGuess = clicked.parentNode.dataset.name
-            console.log(secondGuess)
-            clicked.parentNode.classList.add('selected')
-        }
-        // If both guesses are not empty...
-        if (firstGuess !== '' && secondGuess !== '') {
-            // and the first guess matches the second match...
-            if (firstGuess === secondGuess) {
-                setTimeout(match, delay)
-                setTimeout(resetGuesses, delay)
-            } else {
-                setTimeout(resetGuesses, delay)
-            }
-        }
 
-        // Set previous target to clicked
-        previousTarget = clicked;
-    }
-})
+// Do not allow the grid section itself to be selected; only select divs inside the grid
+
+
+// Add selected class
+// ...
+
+// Add selected class
+
+// Assign first guess
+
+// Assign second guess
+
+// If both guesses are not empty...
+
+// and the first guess matches the second match...
+
+
+// Set previous target to clicked
+
 
 // Add match CSS
-const match = () => {
-    var selected = document.querySelectorAll('.selected')
-    selected.forEach(card => {
-        card.classList.add('match')
-    })
-}
-
-const resetGuesses = () => {
-    firstGuess = ''
-    secondGuess = ''
-    count = 0
-
-    var selected = document.querySelectorAll('.selected')
-    selected.forEach(card => {
-        card.classList.remove('selected')
-    })
-}
